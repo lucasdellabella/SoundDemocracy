8f9dcf15c2a32429bcdb539aa9c45b372d2b4d77
Lucas Della Bella <dellabella.lucas@gmail.com>
2018-06-06 19:25:18 -0700

    Add playing / pausing actual files from S3

--- app/components/sound-element.js
+++ app/components/sound-element.js
@@ -1,15 +1,48 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    init: function(fileUrl) {
+        this._super()
+        this.setProperties({
+            fileUrl 
+        })
+    },
+
+    hifi: Ember.inject.service(),
+
     isPlaying: false,
 
-    playPauseSourceImg: Ember.computed('isPlaying', function() {
-        return this.get('isPlaying') ? 'play_button.png' : 'pause_button.png';
-    }),
+    fileUrl: null,
+
+    playPauseSourceImg: Ember.computed(
+        'hifi.currentSound.url', 
+        'hifi.currentSound.isPlaying',
+        'sound.url', 
+        function() {
+            let thisSoundIsPlaying = this.get('hifi.currentSound.url') == this.get('sound.url') && this.get('hifi.currentSound.isPlaying')
+            return thisSoundIsPlaying ? 'pause_button.png' : 'play_button.png' ;
+        }
+    ),
 
     actions: {
-        toggleIsPlaying: function() {
+        toggleIsPlaying: function(urls) {
             this.toggleProperty('isPlaying')
+            if (this.get('isPlaying')) {
+                console.log('play')
+                this.get('hifi').play(urls).then(({ sound }) => {
+                // sound object
+
+                }).catch(error => {
+
+                })
+            } else {
+                console.log('pause')
+                this.get('hifi').pause()
+            }
+        },
+
+        play: function(urls) {
         }
+
     }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -6,10 +6,12 @@ export default Route.extend({
             name: 'sound name',
             uploadedBy: 'john doe',
             data: 'Sample sound',
+            url: 'https://s3.amazonaws.com/www.sound-democracy.xyz/samples/EDM-6.mp3'
         }, {
             name: 'sound name',
             uploadedBy: 'john doe',
             data: 'Sample sound',
+            url: 'https://s3.amazonaws.com/www.sound-democracy.xyz/samples/Renegade-Heart.mp3'
         }]
     }
 });
